a9bbe1132e865fc5f85f3f3d13bc9633b3fdb13a
Magnus Nyborg <manny@live.no>
2026-02-13 23:14:55 +0100

    Testing av vscode

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -15,7 +15,7 @@ const Index = () => {
             className="h-24 w-24 rounded-full object-cover border-2 border-border"
           />
           <h1 className="text-2xl font-bold tracking-tight">Evig_eventyr</h1>
-          <p className="text-sm text-muted-foreground text-center">Hey there, get in touch with me here (or just look at the pretty pictures, I don't mind)</p>
+          <p className="text-sm text-muted-foreground text-center">Hey there, get in touch with me here (or just look at the pretty pictures, I don't mind) </p>
         </div>
 
         {/* Status */}
